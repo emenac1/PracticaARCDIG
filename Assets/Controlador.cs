74db9fc327d71ae2130650a19ecf4aab9d03f047
emenac <e.menac@alumnos.upm.es>
2022-05-03 12:39:45 +0200

    commit inicial

---
+++ Assets/Controlador.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controlador : MonoBehaviour {
+
+	//EN DefaultTrackableEventHandler POSIBLEMENTE ESTÉ EL FALLO, MIRAR
+	private Vector3[] balones;
+	bool bolaCreada;
+	public GameObject bola;
+	// Use this for initialization
+	void Start () {
+		balones = new Vector3[5];
+		bolaCreada = false;
+		bola = GameObject.Find ("Bola");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!bolaCreada && balones [0] != Vector3.zero) {
+			bola = new GameObject ("Bola");
+			bola.SetActive (true);
+			 
+		}
+
+			
+	}
+
+	public void nuevoBalon (Vector3 posicion) {
+		bool noCreado = true;
+		for (int i = 0; i < 5 && noCreado; i++) {
+			if (balones [i] == Vector3.zero){
+				balones [i] = posicion;
+				noCreado = false;
+			}
+			print (balones [i]);
+		}
+
+	}
+
+	public void borrarBalon (Vector3 posicion) {
+		bool noBorrado = true;
+		for (int i = 0; i < 5 && noBorrado; i++) {
+			if (balones [i] == posicion){
+				balones [i] = Vector3.zero;
+				noBorrado = false;
+			}
+			print (balones [i]);
+		}
+
+	}
+		
+}
---
+++ Assets/movimientoBola.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimientoBola : MonoBehaviour {
+	public Rigidbody rb;
+	public Vector3[] ruta;
+	public float velocidadVertical;
+	public int actual;
+	public bool botado;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		ruta = new Vector3[5];
+		for (int i = 0; i < 5; i++) {
+			ruta [i] = Vector3.zero;
+		}
+		velocidadVertical = 40;
+		actual = 0;
+		botado = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y <= 0) {
+			velocidadVertical = 40;
+			if (!botado) {
+				botado = true;
+				rb.velocity = new Vector3 (0, velocidadVertical, 0);
+			} else {
+				
+				int siguiente = 0;
+				for (int i = actual+1; i < actual+6; i++) {
+					if (ruta [i % 5] != Vector3.zero) {
+						siguiente = i%5;
+					}
+				}
+
+				float velocidadX=(ruta[siguiente].x-ruta[actual].x)*20;
+				float velocidadZ=(ruta[siguiente].z-ruta[actual].z)*20;
+				actual = siguiente;
+				rb.velocity = new Vector3 (velocidadX, velocidadVertical, velocidadZ);
+				botado = false;
+			}
+		}
+
+		
+	}
+	public void setRuta(Vector3[] ruta){
+		this.ruta = ruta;
+	}
+}
