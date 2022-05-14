d387916893382b2b85290dc75201558afdece0d9
enriquer47 <enrique.martin.gutierrez@alumnos.upm.es>
2022-05-14 17:58:20 +0200

    Listo para pruebas con cilindro

---
+++ Assets/Controlador.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controlador : MonoBehaviour {
+
+	//EN DefaultTrackableEventHandler POSIBLEMENTE ESTÉ EL FALLO, MIRAR
+	private Vector3[] balones;
+	bool bolaCreada;
+	public static Controlador controlador;
+
+	public GameObject Bola;
+	public GameObject instanciaBola;
+	public Vector3 redbull;
+	// Use this for initialization
+
+	public void Awake(){
+		redbull = Vector3.zero;
+		controlador = this;
+		balones = new Vector3[4];
+		bolaCreada = false;
+		//bola = GameObject.Find ("Bola");
+		for (int i = 0; i < 4; i++)
+			balones [i] = Vector3.zero;
+	}
+
+
+	void Start () {
+		balones = new Vector3[4];
+		bolaCreada = false;
+		redbull = Vector3.zero;
+		//bola = GameObject.Find ("Bola");
+		for (int i = 0; i < 4; i++)
+			balones [i] = Vector3.zero;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+					
+	}
+
+	public int nuevoBalon (Vector3 posicion) {
+		bool noCreado = true;
+		int pos = -1;
+		if (!bolaCreada) {
+			instanciaBola = (GameObject)Instantiate(Bola, posicion, Quaternion.identity);
+			bolaCreada = true;
+		}
+		print ("la lista de balones es: ");
+		for (int i = 0; i < 4 && noCreado; i++) {
+			if (balones [i] == Vector3.zero){
+				balones [i] = posicion;
+				noCreado = false;
+				pos = i;
+			}
+
+			print (balones [i]);
+		}
+		instanciaBola.GetComponent<movimientoBola>().setRuta(balones);
+		return pos;
+
+	}
+
+	public void borrarBalon (int posicion) {
+		bool noVacio = false;
+		if(posicion != -1)
+			balones [posicion] = Vector3.zero;
+		for (int i = 0; i < 4 && !noVacio; i++) { 
+			if (balones [i] != Vector3.zero)
+				noVacio = true;
+		}
+		if(!noVacio){
+			bolaCreada = false;
+			Destroy(instanciaBola);
+			print ("bola borrada");
+		} else {
+			Bola.GetComponent<movimientoBola>().setRuta (balones);
+			print ("balon borrado");
+		}
+
+	}
+
+	public void actualizarPosicion(int posicion, Vector3 coordenadas){
+		if (posicion != -1) {
+			balones [posicion] = coordenadas; 
+			Bola.GetComponent<movimientoBola>().setRuta (balones);
+		}
+	}
+	public void crearRedbull(Vector3 posicion)
+    {
+		redbull = posicion;
+		Bola.GetComponent<movimientoBola>().setRedbull(posicion);
+    }
+	public void borrarRedbull()
+	{
+		redbull = Vector3.zero;
+		Bola.GetComponent<movimientoBola>().setRedbull(Vector3.zero);
+	}
+
+}
---
+++ Assets/movimientoBola.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimientoBola : MonoBehaviour {
+	public Rigidbody rb;
+	public Vector3[] ruta;
+	public float velocidadVertical;
+	public int actual;
+	public bool botado;
+	private int siguiente;
+	private int cont;
+	private float suelo;
+	private bool finalizado;
+
+
+	// Use this for initialization
+
+	public void Awake()
+	{
+		finalizado = false;
+		ruta = new Vector3[5];
+		for (int i = 0; i < 5; i++)
+			ruta[i] = Vector3.zero; 
+		
+	}
+
+	void Start () {
+		print ("bola creada");
+		rb = GetComponent<Rigidbody> ();
+		ruta = new Vector3[5];
+		for (int i = 0; i < 5; i++) {
+			ruta [i] = Vector3.zero;
+		}
+		velocidadVertical = 2000;
+		actual = 0;
+		suelo = 0;
+		botado = false;
+		siguiente = 0;
+		cont = 200;
+		finalizado = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+		if (transform.position.y <= 0 && cont < 0)
+		{
+			if (finalizado)
+			{
+				rb.velocity = new Vector3(0, 0, 0);
+			}
+			else
+			{
+				cont = 200;
+				rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+				rb.AddForce(transform.up * velocidadVertical);
+
+				//transform.position = new Vector3(transform.position.x, transform.position.y+0.1f, transform.position.z);
+
+
+				if (!botado)
+				{
+					botado = true;
+					print("No paraboleo y pongo botado a true");
+					rb.velocity = new Vector3(0, rb.velocity.y, 0);
+				}
+				else
+				{
+
+					bool encontrado = false;
+					for (int i = actual + 1; i < actual + 5 && !encontrado; i++)
+					{
+						if (ruta[i % 4] != Vector3.zero)
+						{
+							siguiente = i % 4;
+							encontrado = true;
+						}
+
+					}
+					print("Actual=" + actual + "Siguiente=" + siguiente);
+					if (actual != siguiente)
+					{
+						bool noVacio = true;
+						if (siguiente == 0)
+						{
+							for (int i = 0; i < 5 && noVacio; i++)
+							{
+								if (ruta[i] == Vector3.zero)
+									noVacio = false;
+							}
+							if (noVacio)
+							{
+								siguiente = 4;
+								finalizado = true;
+							}
+						}
+
+						print("Paraboleo y pongo botado a false");
+						botado = false;
+						print("estoy en ruta de actual " + ruta[actual]);
+						print("estoy en ruta de siguiente " + ruta[siguiente]);
+
+						rb.AddForce((ruta[siguiente].x - ruta[actual].x) * 6, 0, (ruta[siguiente].z - ruta[actual].z) * 6);
+						actual = siguiente;
+						suelo = ruta[actual].y;
+
+					}
+				}
+				
+			}
+		
+		}
+		cont--;
+
+
+
+	}
+	public void setRuta(Vector3[] balones){
+		for(int i=0; i<4; i++)
+        {
+			Vector3 aux = new Vector3( balones[i].x,balones[i].y,balones[i].z);
+			ruta[i] = aux;
+        }
+		
+		print ("esta es la ruta: ");
+		for (int i = 0; i < 5; i++)
+			print (ruta [i]);
+	}
+	public void setRedbull(Vector3 posicion)
+	{
+		ruta[4] = posicion;
+	}
+
+
+
+}
